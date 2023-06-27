7d811ac3af79ef57cb4cf1ed053006b3697da57f
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-06-27 17:44:40 -0300

    agregar texto mañana

---
+++ main.js
@@ -0,0 +1,46 @@
+/*//ejercicicos de  push y pop
+
+let frutas = [];
+console.log(frutas)
+frutas.push("manzana")
+console.log(frutas)
+frutas.push("naranja","banana");
+console.log(frutas)
+frutas.pop()
+console.log(frutas)
+
+
+// ejercicios join y split
+
+
+*/
+
+// let nombre = ["Alberto Fernandez","Maria Garcia","Pedro Sanchez"];
+// console.log(nombre);
+
+// let nombreCompleto = nombre.join(", ");
+// console.log(nombreCompleto);
+
+// let arrayNombreCompleto = nombreCompleto.split("-");
+
+// console.log(arrayNombreCompleto)
+
+
+
+
+// Ejercicio de slice y splice:
+
+let numeros = [1,2,3,4,5];
+console.log(numeros);
+
+let subArray = numeros.slice(2)
+console.log(subArray)
+
+let eliminar = numeros.splice()
+console.log(eliminar)
+
+
+
+
+
+
